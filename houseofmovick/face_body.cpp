f3ad29dd68d34690dd78b3f2acd6f32644b8a525
justin movick baby baby <88159562+movickmovick@users.noreply.github.com>
2021-08-17 13:39:10 -0600

    Add face_body.cpp

---
+++ houseofmovick/face_body.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+<struct face>
+
+string color;
+string emotion;
+bool messy;
+
+</struct face>
+
+<struct body>
+
+bool chubby;
+bool puny;
+bool spotted;
+bool hashed;
+
+</struct body>
